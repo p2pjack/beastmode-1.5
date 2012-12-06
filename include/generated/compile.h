234fea7dd6fb9987faeb5818853d4b2107a76381
p2pjack <p2pjack@gmail.com>
2012-12-06 12:19:14 +0000

    Beastmaode 1.5 by sbryan12144 with headset fix by ucxl

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 10 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#10 PREEMPT Sun Dec 2 12:32:05 GMT 2012"
+#define LINUX_COMPILE_BY "p2pjack"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILER "gcc version 4.6.2 20111004 (prerelease) (Linaro GCC 4.6-2011.10) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.0.41-BeastMode-v1.5-Headset-Test+"
