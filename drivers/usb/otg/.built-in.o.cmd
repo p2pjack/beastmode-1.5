c605326a56c9c9691b56cde3b2d6a256da192e74
p2pjack <p2pjack@gmail.com>
2012-12-07 02:05:03 +0000

    Beastmaode 1.5 by sbryan12144 with headset fix by ucxl

---
+++ drivers/usb/otg/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/usb/otg/built-in.o :=  /home/p2pjack/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-ld -EL    -r -o drivers/usb/otg/built-in.o drivers/usb/otg/otg.o drivers/usb/otg/otg_id.o drivers/usb/otg/msm72k_otg.o 
