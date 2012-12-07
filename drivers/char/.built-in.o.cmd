4a79b68ef07d724bd1bdbbf86e022df387227994
p2pjack <p2pjack@gmail.com>
2012-12-07 02:36:57 +0000

    Beastmaode 1.5 by sbryan12144 with headset fix by ucxl

---
+++ drivers/char/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/char/built-in.o :=  /home/p2pjack/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-ld -EL    -r -o drivers/char/built-in.o drivers/char/mem.o drivers/char/random.o drivers/char/misc.o drivers/char/msm_rotator.o 
