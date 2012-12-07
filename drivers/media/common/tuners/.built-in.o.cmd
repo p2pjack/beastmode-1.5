b2fbe4b6d057b1b8131441d3a36683e694148216
p2pjack <p2pjack@gmail.com>
2012-12-07 02:25:07 +0000

    Beastmaode 1.5 by sbryan12144 with headset fix by ucxl

---
+++ drivers/media/common/tuners/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/media/common/tuners/built-in.o :=  rm -f drivers/media/common/tuners/built-in.o; /home/p2pjack/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-ar rcsD drivers/media/common/tuners/built-in.o
---
+++ drivers/media/video/msm/rawchip/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/media/video/msm/rawchip/built-in.o :=  rm -f drivers/media/video/msm/rawchip/built-in.o; /home/p2pjack/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-ar rcsD drivers/media/video/msm/rawchip/built-in.o
