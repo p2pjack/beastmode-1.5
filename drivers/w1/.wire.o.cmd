03764542c8ed3e7d629919f867a4fb4dfbd1af25
p2pjack <p2pjack@gmail.com>
2012-12-07 02:02:00 +0000

    Beastmaode 1.5 by sbryan12144 with headset fix by ucxl

---
+++ drivers/w1/.wire.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/w1/wire.o := /home/p2pjack/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-ld -EL    -r -o drivers/w1/wire.o drivers/w1/w1.o drivers/w1/w1_int.o drivers/w1/w1_family.o drivers/w1/w1_netlink.o drivers/w1/w1_io.o 
---
+++ drivers/w1/masters/.built-in.o.cmd
@@ -0,0 +1,1 @@
+cmd_drivers/w1/masters/built-in.o :=  rm -f drivers/w1/masters/built-in.o; /home/p2pjack/android/kernel/toolchains/arm-eabi-linaro-4.6.2/bin/arm-eabi-ar rcsD drivers/w1/masters/built-in.o
