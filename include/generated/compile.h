073637ccddabb089f32a261d9a003f8b2de61393
p2pjack <p2pjack@gmail.com>
2012-12-16 21:26:28 +0000

    clean and fix

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 3 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#3 PREEMPT Sun Dec 16 00:29:19 GMT 2012"
+#define UTS_VERSION "#5 PREEMPT Sun Dec 16 18:44:31 GMT 2012"
 #define LINUX_COMPILE_BY "p2pjack"
 #define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
+#define LINUX_COMPILER "gcc version 4.6.4 20120630 (prerelease) (Linaro GCC 4.6-2012.07) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.0.56-BeastMode-v1.5-MOD-V0.2-g05ef6aa-dirty"
+#define UTS_RELEASE "3.0.56-BeastMode-v1.5-MOD-V0.2-g2b02a86-dirty"
