fab19ea674f758c5d4c60ef6fe54214d87ae26bb
Thạch Tùng Anh <thachtunganh@Gmail.com>
2012-12-25 12:19:24 +0700

    Clean up.

--- include/generated/compile.h
+++
@@ -1,7 +0,0 @@
-/* This file is auto generated, version 11 */
-/*  PREEMPT */
-#define UTS_MACHINE "arm"
-#define UTS_VERSION "#11 PREEMPT Sat Dec 22 01:18:35 GMT 2012"
-#define LINUX_COMPILE_BY "p2pjack"
-#define LINUX_COMPILE_HOST "ubuntu"
-#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
--- include/generated/utsrelease.h
+++
@@ -1,1 +0,0 @@
-#define UTS_RELEASE "3.0.57-BeastMode-v1.5-MOD-V0.3.1-g3059c86-dirty"
